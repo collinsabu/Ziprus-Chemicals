a7cd06c7616f019064f0245197b635a3d7c75be9
Collins Abu <64439462+collinsabu@users.noreply.github.com>
2025-10-03 14:16:15 +0100

    i change the phone number

--- app/components/Header.js
+++ app/components/Header.js
@@ -66,7 +66,7 @@ const Header = () => {
       {/* Top Bar */}
       <div className="top-bar flex justify-between items-center px-5 sm:px-7 bg-base_two h-10 text-base_text fixed w-full z-50">
         <div>
-          <h6 className="font-semibold text-sm">+2342018899613</h6>
+          <h6 className="font-semibold text-sm">+2347085544340</h6>
         </div>
         <div className="socials flex items-center gap-2">
           <a

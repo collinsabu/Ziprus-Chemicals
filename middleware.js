9d0a821380a8a9d5581a017e40adfe28fb0bad35
Collins Abu <64439462+collinsabu@users.noreply.github.com>
2026-01-29 21:19:38 +0000

    updated phonebook route

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = req.nextUrl;
 
-  const adminRoutes = ['/admin/orderlist','/admin/contactlist','/balance','/balance/workerswages', '/balance/stockbalance', '/viewloadingandpayment','/loadingandpayment/custmerpayment', '/e-learning/adminLevelTwo', '/e-learning/marketing', '/e-learning/secretary', '/report/crudepayment', '/business-partners', '/expenses'];
+  const adminRoutes = ['/admin/orderlist','/admin/contactlist','/balance','/balance/workerswages', '/balance/stockbalance', '/viewloadingandpayment','/loadingandpayment/custmerpayment', '/e-learning/adminLevelTwo', '/e-learning/marketing', '/e-learning/secretary', '/report/crudepayment', '/expenses'];
   const userRoutes = [
     '/admin', 
     '/admin/assets',
